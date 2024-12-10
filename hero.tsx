d3493572903c90764d8606d4613450714413ab41
Usama Nadeem <161374083+Usama2745@users.noreply.github.com>
2024-12-10 22:35:44 +0500

    Add files via upload

---
+++ hero.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Image from "next/image";
+
+function hero() {
+  return (
+    <div className='grid grid-cols-1'>
+        <div>
+        <Image src="/unsplash_4ycv3Ky1ZZU.png" alt="logo" width={1920} height={410} />
+        <div className='text-black w-[219px] h-[56px] top-[198px] left-[850px]'>
+              
+        </div>
+    </div>
+    </div>
+  )
+}
+
+export default hero
---
+++ page.tsx
@@ -0,0 +1,13 @@
+import Cards from "./components/cards";
+import Header from "./components/header";
+import Hero from "./components/hero";
+
+export default function Home() {
+  return (
+    <div>
+      <Header />
+      <Hero />
+      <Cards />
+      </div>
+  );
+}
